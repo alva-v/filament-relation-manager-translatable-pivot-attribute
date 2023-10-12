c9166e0f9362f5e3e61f6ecb4da73a260f613abe
Alva Vially <alva@vially.fr>
2023-10-12 15:53:04 +0200

    Create migration

---
+++ database/migrations/2023_10_12_134751_create_demo_tables.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foos', function (Blueprint $table) {
+            $table->id();
+            $table->json('name');
+            $table->timestamps();
+        });
+
+        Schema::create('bars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('bar_foo', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bar_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('foo_id')->constrained()->cascadeOnDelete();
+            $table->string('pivot_attribute')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bar_foo');
+        Schema::dropIfExists('foos');
+        Schema::dropIfExists('bars');
+    }
+};
